a2fac9ccaae2057765e80dd14b12c73c7965504b
Richies Luong <you@example.com>
2022-01-24 16:08:22 -0500

    HackerRank Challenges

---
+++ src/com/company/Day6.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.io.*;
+import java.util.*;
+
+public class Day6 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        String s = "Hacker";
+        char[] charArray = s.toCharArray();
+        String even = "", odd = "";
+        int numChar;
+        for (int i = 0; i < charArray.length; i++) {
+            if (i % 2 == 0) {
+                even += charArray[i];
+            } else {
+                odd += charArray[i];
+            }
+        }
+        System.out.print(even + " " + odd);
+    }
+
+    public static String splitEvenAndOdd(String str) {
+
+        char[] charArray = str.toCharArray();
+        String even = "", odd = "";
+
+        for (int i = 0; i < charArray.length; i++) {
+            if ( i % 2 == 0) {
+                even += charArray[i];
+            } else {
+                odd += charArray[i];
+            }
+        }
+        return even + " " + odd;
+    }
+}
