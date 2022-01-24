7af0505209726c356ec18c722e7538aa39510b65
Richies Luong <you@example.com>
2022-01-24 16:21:59 -0500

    Day)

--- src/com/company/Day6.java
+++ src/com/company/Day6.java
@@ -8,6 +8,8 @@ public class Day6 {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
 
+        System.out.println(splitEvenAndOdd("adbecf"));
+
         String s = "Hacker";
         char[] charArray = s.toCharArray();
         String even = "", odd = "";
