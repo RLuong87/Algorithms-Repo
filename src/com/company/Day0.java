0f06ec43359924fcc8bc4afd62c0c7e9c1ecc43f
Richies Luong <87341263+RLuong87@users.noreply.github.com>
2022-01-24 16:22:34 -0500

    Day0

--- src/com/company/Day0.java
+++ src/com/company/Day0.java
@@ -1,4 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Day0 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        // Create a Scanner object to read input from stdin.
+        Scanner scan = new Scanner(System.in);
+
+        // Read a full line of input from stdin and save it to our variable, inputString.
+        String inputString = scan.nextLine();
+
+        // Close the scanner object, because we've finished reading
+        // all of the input from stdin needed for this challenge.
+        scan.close();
+
+        // Print a string literal saying "Hello, World." to stdout.
+        System.out.println("Hello, World.");
+
+        // TODO: Write a line of code here that prints the contents of inputString to stdout.
+        System.out.println(inputString);
+    }
 }
