8a1ddb9dabe03321ffe426e407f3235993cf2de6
Richies Luong <you@example.com>
2022-02-08 14:29:28 -0500

    Sales By Match HackerRank Challenge

--- src/com/company/SalesByMatch.java
+++ src/com/company/SalesByMatch.java
@@ -1,4 +1,54 @@
 package com.company;
 
+import java.util.*;
+
 public class SalesByMatch {
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+
+        int totalPairs = 0;
+
+        HashMap<Integer, Integer> sockCount = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            sockCount.put(ar.get(i), Collections.frequency(ar, ar.get(i)));
+        }
+
+        for (Map.Entry<Integer, Integer> entry : sockCount.entrySet()) {
+            totalPairs += entry.getValue() / 2;
+        }
+        return totalPairs;
+    }
+
+    public static void main(String[] args) {
+
+//        List<Integer> pairs = List.of(1, 2, 1, 2, 1, 3, 2);
+        List<Integer> pairs = List.of(10, 20, 20, 10, 10, 30, 50, 10, 20);
+
+        System.out.println(sockMerchant(pairs.size(), pairs));
+//        List<Integer> pairs = new ArrayList<>() {{
+//            add(10);
+//            add(20);
+//            add(20);
+//            add(20);
+//            add(10);
+//        }};
+//        System.out.println(sockMerchant(pairs.size(), pairs));
+
+        HashMap<Integer, Integer> sockCount = new HashMap<>();
+
+        for (int i = 0; i < pairs.size(); i++) {
+
+            sockCount.put(pairs.get(i), Collections.frequency(pairs, pairs.get(i)));
+
+        }
+
+        int total = 0, totalPairs = 0;
+        for (Map.Entry<Integer, Integer> entry : sockCount.entrySet()) {
+            total = entry.getValue() / 2;
+            totalPairs += total;
+        }
+//        System.out.println(totalPairs);
+        System.out.println("Number of socks: " + sockCount);
+    }
 }
