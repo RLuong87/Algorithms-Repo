b9e600cac32d6bdee09817216f9c6e843c7b7df9
Richies Luong <you@example.com>
2022-02-14 11:19:43 -0500

    Jumping on Clouds HackerRank Challenge

--- src/com/company/JumpingOnClouds.java
+++ src/com/company/JumpingOnClouds.java
@@ -13,9 +13,9 @@ public class JumpingOnClouds {
         do {
             cloud = (cloud + distance) % clouds.length;
             if (clouds[cloud] == 0) {
-                health = health - 1;
+                health--;
             } else {
-                health = health - 3;
+                health-= 3;
             }
         } while (cloud != 0);
 
@@ -24,11 +24,11 @@ public class JumpingOnClouds {
 
     public static void main(String[] args) {
 
-//        List<Integer> clouds = Arrays.asList(1, 1, 1, 0, 1, 1, 0, 0, 0, 0); // 1, 1, 1, 0, 1, 1, 0, 0, 0, 0 / Expected output: 80 Health remaining
-        int[] clouds = {1, 1, 1, 0, 1, 1, 0, 0, 0, 0}; // 0, 0, 1, 0, 0, 1, 1, 0 / Expected output: 80 Health remaining
+//        int[] clouds = {1, 1, 1, 0, 1, 1, 0, 0, 0, 0}; // 1, 1, 1, 0, 1, 1, 0, 0, 0, 0 / Expected output: 80 Health remaining
+        int[] clouds = {0, 0, 1, 0, 0, 1, 1, 0}; // 0, 0, 1, 0, 0, 1, 1, 0 / Expected output: 92 Health remaining
         int health = 100, distance = 2;
 
-        System.out.println(jumpingOnClouds(3, clouds, 100));
+        System.out.println(jumpingOnClouds(2, clouds, 100));
 
 
         for (int i = 0; i < clouds.length - distance; i++) {
