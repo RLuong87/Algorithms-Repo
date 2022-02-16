99d1113062937a12195029979f8f64064ac50330
Richies Luong <you@example.com>
2022-02-16 16:43:39 -0500

    HackerRank Algorithms

--- src/com/company/JumpingOnClouds.java
+++ src/com/company/JumpingOnClouds.java
@@ -20,26 +20,28 @@ public class JumpingOnClouds {
 
     public static void main(String[] args) {
 
-        int[] clouds = {1, 1, 1, 0, 1, 1, 0, 0, 0, 0}; // 1, 1, 1, 0, 1, 1, 0, 0, 0, 0 / k = 3 / Expected output: 80 Health remaining
-//        int[] clouds = {0, 0, 1, 0, 0, 1, 1, 0}; // 0, 0, 1, 0, 0, 1, 1, 0 / k = 2 / Expected output: 92 Health remaining
+//        int[] clouds = {1, 1, 1, 0, 1, 1, 0, 0, 0, 0}; // 1, 1, 1, 0, 1, 1, 0, 0, 0, 0 / k = 3 / Expected output: 80 Health remaining
+        int[] clouds = {0, 0, 1, 0, 0, 1, 1, 0}; // 0, 0, 1, 0, 0, 1, 1, 0 / k = 2 / Expected output: 92 Health remaining
+
+        System.out.println(jumpingOnClouds(2, clouds));
+
         int energy = 100, distance = 2;
 
-        System.out.println(jumpingOnClouds(3, clouds));
+        for (int cloud = 0; cloud < clouds.length; cloud++) {
+            cloud = (cloud + distance) % clouds.length;
 
+            if (clouds[cloud] == 0) {
+                energy--;
+            }
 
+            if (clouds[cloud] == 1) {
+                energy -= 3;
+            }
 
-
-
-
-        for (int i = 0; i < clouds.length - distance; i++) {
-            int jump = i + distance, numOfClouds = clouds.length;
-
-            if (clouds[jump] % numOfClouds == 0) {
-                energy = energy - 1;
-            } else if (clouds[jump] % numOfClouds != 0) {
-                energy = energy - 3;
+            if (cloud == 0) {
+                break;
             }
         }
-//        System.out.println(health);
+        System.out.println(energy);
     }
 }
--- src/com/company/SherlockAndAnagrams.java
+++ src/com/company/SherlockAndAnagrams.java
@@ -11,14 +11,12 @@ public class SherlockAndAnagrams {
 
     public static int sherlockAndAnagrams(String s) {
 
-        int count = 0, length = s.length();
+        int count = 0;
         String str1 = s.substring(0, 3), str2 = s.substring(1);
         String str3 = s.substring(0, 2), str4 = s.substring(2);
         String str5 = s.substring(1, 2), str6 = s.substring(2, 3);
 
         // if each char in a position
-
-        if (s.charAt(0) == s.charAt(length - 1)) count++;
 
         if (s.contains(str1) && s.contains(str2)) count++;
 
@@ -26,12 +24,6 @@ public class SherlockAndAnagrams {
 
         if (s.contains(str5) && s.contains(str6)) count++;
 
-
-//        System.out.println(count);
-        System.out.println(str1 + ", " + str2);
-        System.out.println(str3 + ", " + str4);
-        System.out.println(str5 + ", " + str6);
-//        System.out.println(count2);
         return count;
     }
 
@@ -39,29 +31,15 @@ public class SherlockAndAnagrams {
 
         System.out.println(sherlockAndAnagrams("abba"));
 
-
         String str = "abba";
 
+//        HashMap<Integer>
 
-        char[] c = str.toCharArray();
-
-        String s = "";
-        int length = str.length(), count = 0;
-
-
-        if (str.charAt(0) == str.charAt(length - 1)) {
-            count++;
-        }
-
-//        if (str.substring(0, 2) == str.substring(1)) {
-//            count++;
-//        }
 
 //        System.out.println(count);
 //        System.out.println(str.substring(0, str.length() - 1));
-        System.out.println(str.substring(0, 1));
+//        System.out.println(str.substring(0, 1));
 //        System.out.println(str.charAt(0));
-        System.out.println(str.substring(1));
-
+//        System.out.println(str.substring(1));
     }
 }
