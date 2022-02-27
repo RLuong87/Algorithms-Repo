6a2d17657abc49c13cbbec6d6b7cda208358e766
Richies Luong <richiesluong@gmail.com>
2022-02-26 23:48:06 -0500

    Day 9

--- src/com/company/hackerrank/RansomNote.java
+++ src/com/company/hackerrank/RansomNote.java
@@ -7,32 +7,63 @@ public class RansomNote {
     public static void checkMagazine(List<String> magazine, List<String> note) {
 
         HashMap<String, String> checkMagazine = new HashMap<>();
+        HashMap<String, String> checkNote = new HashMap<>();
 
+        for (String m : magazine) {
 
+            for (String n : note) {
+                checkMagazine.put(m, m);
+                checkNote.put(n, n);
+            }
+        }
+        StringBuilder yes = new StringBuilder();
+
+        for (String cn : checkNote.values()) {
+
+//            for (Map.Entry<String, String> cm : checkMagazine.entrySet()) {
+            for (String cm : checkNote.values()) {
+
+                if (cn.equals(cm)) {
+                    yes.append("Yes");
+                } else {
+                    yes.append("No");
+                }
+            }
+        }
+        System.out.println(yes);
+        System.out.println(checkMagazine);
+        System.out.println(checkNote);
     }
 
     public static void main(String[] args) {
 
-        HashMap<Integer, String> checkMagazine = new HashMap<>();
-//        HashMap<Integer, String> ransomNote = new HashMap<>();
-
         String mag = "attack at dawn", rNote = "Attack at dawn";
+//        String mag = "give me one grand today night", rNote = "give one grand today";
 
         List<String> magazine = new ArrayList<>(Arrays.asList(mag.split(" ")));
         List<String> noteR = new ArrayList<>(Arrays.asList(rNote.split(" ")));
+        checkMagazine(magazine, noteR);
+
+        HashMap<Integer, String> checkMagazine = new HashMap<>();
+//        HashMap<Integer, String> ransomNote = new HashMap<>();
+
 
         for (int i = 0; i < magazine.size(); i++) {
             checkMagazine.put(i, magazine.get(i));
         }
 
-        for (String notes : checkMagazine.values()) {
+        boolean check = false;
 
-            for (String s : noteR) {
+        for (String s : noteR) {
 
-                if (notes.equals(s)) {
-                    System.out.println("Yes");
+            for (String notes : checkMagazine.values()) {
+
+                if (s.equals(notes)) {
+                    check = true;
+//                    System.out.println("Yes");
                 } else {
-                    System.out.println("No");
+                    check = true;
+//                    System.out.println("No");
                 }
             }
         }
@@ -41,7 +72,7 @@ public class RansomNote {
 //            ransomNote.put(i, noteR.get(i));
 //        }
 
-        System.out.println(checkMagazine);
+//        System.out.println(checkMagazine);
 //        System.out.println(ransomNote);
     }
 }
--- src/com/company/hackerrankwarmups/DiagonalDifference.java
+++ src/com/company/hackerrankwarmups/DiagonalDifference.java
@@ -1,18 +1,30 @@
 package com.company.hackerrankwarmups;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DiagonalDifference {
 
     public static int diagonalDifference(List<List<Integer>> arr) {
-        // Write your code here
 
+        for (int i = 0; i < arr.size(); i++) {
+
+            for (int j = 0; j < arr.size(); j++) {
+
+
+            }
+        }
         return 0;
     }
 
     public static void main(String[] args) {
 
+        List<List<Integer>> arr = new ArrayList<>();
+        arr.add(List.of(1, 2, 3));
+        arr.add(List.of(4, 5, 6));
+        arr.add(List.of(9, 8, 9));
 
+        System.out.println(diagonalDifference(arr));
 
     }
 }
