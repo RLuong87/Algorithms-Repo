732c9d483ffb27cbd97a111651b119fdc18dde4a
Richies Luong <richiesluong@gmail.com>
2022-02-28 17:55:39 -0500

    Day 7

--- src/com/company/thirtydaysofcode/Day7.java
+++ src/com/company/thirtydaysofcode/Day7.java
@@ -15,37 +15,41 @@ import static java.util.stream.Collectors.toList;
 
 public class Day7 {
 
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-//
-//        int n = Integer.parseInt(bufferedReader.readLine().trim());
-//
-//        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-//                .map(Integer::parseInt).toList();
-//
-//        for (int i = arr.size() - 1; i > 0 ; i--) {
-//            System.out.print(arr.get(i));
-//        }
-//
-//        bufferedReader.close();
-//    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
 
-        int[] numbers = {1, 4, 3, 2};
-        List<Integer> numbers2 = List.of(1, 2, 3, 4);
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(""))
+                .map(Integer::parseInt).toList();
 
-        displayNumbers(numbers2);
-        System.out.println();
+        for (int i = arr.size() - 1; i >= 0; i--) {
 
-        for (int i = numbers.length - 1; i >= 0; i--) {
-
-            System.out.print(numbers[i]);
+            System.out.print(arr.get(i));
             if (i > 0) {
                 System.out.print(" ");
             }
         }
+
+        bufferedReader.close();
     }
+
+//    public static void main(String[] args) {
+//
+//        int[] numbers = {1, 4, 3, 2};
+//        List<Integer> numbers2 = List.of(1, 2, 3, 4);
+//
+//        displayNumbers(numbers2);
+//        System.out.println();
+//
+//        for (int i = numbers.length - 1; i >= 0; i--) {
+//
+//            System.out.print(numbers[i]);
+//            if (i > 0) {
+//                System.out.print(" ");
+//            }
+//        }
+//    }
 
     public static void displayNumbers(List<Integer> numbers) {
 
