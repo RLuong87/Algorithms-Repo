712a6cc2f323cd2c9b5c5a5ff108d5c1c9549596
Richies Luong <richiesluong@gmail.com>
2022-03-01 18:13:26 -0500

    Day 9

--- src/com/company/thirtydaysofcode/Day9.java
+++ src/com/company/thirtydaysofcode/Day9.java
@@ -1,4 +1,41 @@
 package com.company.thirtydaysofcode;
 
+import java.io.*;
+
+class Results {
+
+    /*
+     * Complete the 'factorial' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int factorial(int n) {
+        // Write your code here
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+}
+
 public class Day9 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Results.factorial(n);
+        System.out.println(result);
+
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedReader.close();
+//        bufferedWriter.close();
+    }
 }
