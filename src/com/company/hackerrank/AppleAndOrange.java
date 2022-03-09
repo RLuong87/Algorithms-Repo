197cc12de10f4d5aab5dae4270b368cda768745e
Richies Luong <richiesluong@gmail.com>
2022-03-08 20:29:49 -0500

    Apple and Orange algorithm

---
+++ src/com/company/hackerrank/AppleAndOrange.java
@@ -0,0 +1,34 @@
+package com.company.hackerrank;
+
+import java.util.List;
+
+public class AppleAndOrange {
+
+    public static void countApplesAndOranges(int s, int t, int a, int b, List<Integer> apples, List<Integer> oranges) {
+
+        int countOfApples = 0, countOfOranges = 0;
+
+        for (Integer apple : apples) {
+            if ((apple + a) >= s && (apple + a) <= t) {
+                countOfApples++;
+            }
+        }
+
+        for (Integer orange : oranges) {
+            if ((orange + b) >= s && (orange + b) <= t) {
+                countOfOranges++;
+            }
+        }
+        System.out.println(countOfApples);
+        System.out.println(countOfOranges);
+    }
+
+    public static void main(String[] args) {
+
+        List<Integer> apples = List.of(2, 3, -4);
+        List<Integer> oranges = List.of(3, -2, -4);
+
+        countApplesAndOranges(7, 10, 4, 12, apples, oranges);
+
+    }
+}
