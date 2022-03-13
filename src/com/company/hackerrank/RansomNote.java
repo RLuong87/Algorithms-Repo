628bc31ce113cb9960ca8da37893267524cc49fc
Richies Luong <richiesluong@gmail.com>
2022-03-12 20:02:01 -0500

    Ransom Note HackerRank

--- src/com/company/hackerrank/RansomNote.java
+++ src/com/company/hackerrank/RansomNote.java
@@ -6,44 +6,48 @@ public class RansomNote {
 
     public static void checkMagazine(List<String> magazine, List<String> note) {
 
-        HashMap<String, String> checkMagazine = new HashMap<>();
-        List<String> notAMatch = new ArrayList<>();
-        List<Integer> countOfWords = new ArrayList<>();
-
-        for (String no : note) {
-           countOfWords.add(Collections.frequency(note, no));
-        }
-
-        for (int count: countOfWords) {
-            if (count > 1) {
-                System.out.println("No");
-            }
-            break;
-        }
+        Map<String, String> checkMagazine = new HashMap<>();
+        Map<String, Integer> countOfWords = new HashMap<>();
 
         for (String m : magazine) {
             checkMagazine.put(m, m);
         }
 
         for (String n : note) {
-            if (!checkMagazine.containsValue(n)) {
-                notAMatch.add(n);
+            if (!countOfWords.containsKey(n)) {
+                countOfWords.put(n, 0);
             }
+            countOfWords.put(n, countOfWords.get(n) + 1);
         }
-        if (!notAMatch.isEmpty()) {
-            System.out.println("No");
-        } else {
-            System.out.println("Yes");
+
+        for (String s : magazine) {
+
+            for (Map.Entry<String, Integer> count : countOfWords.entrySet()) {
+
+                if (!checkMagazine.containsValue(count.getKey())) {
+                    System.out.println("No");
+                    break;
+                } else if (count.getValue() > 1) {
+                    System.out.println("No");
+                    break;
+                } else if (!s.equals(count.getKey())) {
+                    System.out.println("No");
+                    break;
+                } else {
+                    System.out.println("Yes");
+                    break;
+                }
+            }
+            break;
         }
-        System.out.println(notAMatch);
     }
 
     public static void main(String[] args) {
 
 //        String mag = "attack at dawn", rNote = "Attack at dawn";
 //        String mag = "ive got a lovely bunch of coconuts", rNote = "ive got some coconuts";
-//        String mag = "give me one grand today night", rNote = "give one grand today";
-        String mag = "two times three is not four", rNote = "two times two is four";
+        String mag = "give me one grand today night", rNote = "give one grand today";
+//        String mag = "two times three is not four", rNote = "two times two is four";
 
         List<String> magazine = new ArrayList<>(Arrays.asList(mag.split(" ")));
         List<String> noteR = new ArrayList<>(Arrays.asList(rNote.split(" ")));
