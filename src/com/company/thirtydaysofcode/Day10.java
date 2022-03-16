58b5c4793d770a5bb2e8b88ff04047e298c12372
Richies Luong <richiesluong@gmail.com>
2022-03-16 10:24:39 -0400

    Day 10

--- src/com/company/thirtydaysofcode/Day10.java
+++ src/com/company/thirtydaysofcode/Day10.java
@@ -1,4 +1,47 @@
 package com.company.thirtydaysofcode;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Day10 {
+
+    public static void main(String[] args) {
+
+        int n = 125, max = 0;
+        int currentConsecutiveOnes = 0, maxConsecutiveOnes = 0;
+        String bin = Integer.toBinaryString(n);
+
+        for (char digit : bin.toCharArray()) {
+            if (digit == '1') {
+                currentConsecutiveOnes++;
+            } else {
+                if (currentConsecutiveOnes > maxConsecutiveOnes) {
+                    maxConsecutiveOnes = currentConsecutiveOnes;
+                }
+                currentConsecutiveOnes = 0;
+            }
+        }
+
+        if (currentConsecutiveOnes > maxConsecutiveOnes) {
+            maxConsecutiveOnes = currentConsecutiveOnes;
+        }
+
+        System.out.println(maxConsecutiveOnes);
+
+
+        String number = Integer.toBinaryString(n);
+
+        for (String num : number.split("0")) {
+
+            if (num.length() > max) {
+                max = num.length();
+            }
+        }
+        System.out.println(max);
+
+
+        String[] binArray = bin.split("0");
+        System.out.println(Collections.max(Arrays.asList(binArray)).length());
+
+    }
 }
