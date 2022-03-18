6b532d4289ed3ecbce6c27c4a87674eee4383d4f
Richies Luong <richiesluong@gmail.com>
2022-03-18 10:40:17 -0400

    Jumping on the clouds algorithm, solved with Jon Gillard's help

--- src/com/company/hackerrank/JumpingOnClouds.java
+++ src/com/company/hackerrank/JumpingOnClouds.java
@@ -6,27 +6,26 @@ public class JumpingOnClouds {
 
     public static int jumpingOnClouds(List<Integer> c) {
 
-        int countOfJumps = 0;
+        int jumps = 0;
 
-        for (int i = 0; i < c.size() - 1; i++) {
+        for (int i = 0; i != c.size() - 1; i++) {
 
-            if (c.get(i) % 2 == 0 && c.get(i + 1) % 2 == 0) {
-                countOfJumps++;
+            if ((i + 2) <= c.size() - 1 && c.get(i + 2) == 0) {
+                i++;
             }
+            jumps++;
         }
-        System.out.println(c);
-        System.out.println(c.size());
-        return countOfJumps;
+//        System.out.println(c);
+        return jumps;
     }
 
     public static void main(String[] args) {
 
-        System.out.println(jumpingOnClouds(List.of(0, 0, 1, 0, 0, 1, 0)));
-        System.out.println(jumpingOnClouds(List.of(0, 0, 0, 0, 1, 0)));
-        System.out.println(jumpingOnClouds(List.of(0, 0, 1, 0, 0, 0, 0, 1, 0, 0)));
+        System.out.println(jumpingOnClouds(List.of(0, 0, 1, 0, 0, 1, 0))); // -> 4 jumps
+        System.out.println(jumpingOnClouds(List.of(0, 0, 0, 0, 1, 0))); // -> 3 jumps
+        System.out.println(jumpingOnClouds(List.of(0, 0, 1, 0, 0, 0, 0, 1, 0, 0))); // -> 6 jumps
         System.out.println(jumpingOnClouds(List.of(
                 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0,
-                0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0))); //
-
+                0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0))); // Expected output -> 28 jumps
     }
 }
