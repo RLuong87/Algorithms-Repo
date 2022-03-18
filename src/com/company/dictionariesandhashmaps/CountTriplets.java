ddd4fd456cacbac7254f14c1d2ac107dcd24a6cd
Richies Luong <richiesluong@gmail.com>
2022-03-18 13:19:45 -0400

    Solved

---
+++ src/com/company/dictionariesandhashmaps/CountTriplets.java
@@ -0,0 +1,2 @@
+package com.company.dictionariesandhashmaps;public class CountTriplets {
+}
--- src/com/company/dictionariesandhashmaps/RansomNote.java
+++ src/com/company/dictionariesandhashmaps/RansomNote.java
@@ -20,15 +20,13 @@ public class RansomNote {
 
             if (countOfWords.containsKey(n)) {
                 countOfWords.put(n, countOfWords.get(n) - 1);
-                System.out.println("Yes");
-                return;
             }
-            if (countOfWords.get(n) <= 0) {
+            if (countOfWords.get(n) < 0) {
                 System.out.println("No");
                 return;
             }
         }
-        System.out.println(countOfWords);
+        System.out.println("Yes");
     }
 
     public static void checkMagazineV2(List<String> magazine, List<String> note) {
@@ -51,8 +49,8 @@ public class RansomNote {
 
 //        String mag = "attack at dawn", rNote = "Attack at dawn";
 //        String mag = "ive got a lovely bunch of coconuts", rNote = "ive got some coconuts";
-//        String mag = "give me one grand today night", rNote = "give one grand today";
-        String mag = "two times three is not four", rNote = "two times two is four";
+        String mag = "give me one grand today night", rNote = "give one grand today";
+//        String mag = "two times three is not four", rNote = "two times two is four";
         List<String> magazine = new ArrayList<>(Arrays.asList(mag.split(" ")));
         List<String> note = new ArrayList<>(Arrays.asList(rNote.split(" ")));
 
