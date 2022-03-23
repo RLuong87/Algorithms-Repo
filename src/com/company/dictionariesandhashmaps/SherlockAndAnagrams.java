b593337f29e9f140d3877fce55ffa8542eff10c4
Richies Luong <richiesluong@gmail.com>
2022-03-22 21:03:50 -0400

    Not solved

--- src/com/company/dictionariesandhashmaps/SherlockAndAnagrams.java
+++ src/com/company/dictionariesandhashmaps/SherlockAndAnagrams.java
@@ -5,8 +5,10 @@ import java.util.*;
 public class SherlockAndAnagrams {
 
      /*
+
         An anagram is a permutation only applicable to strings. It's a re-arrangement of the characters.
         Two strings are called an anagrammatic pair when they have the same length and the same set of characters.
+
      */
 
     public static int sherlockAndAnagrams(String s) {
