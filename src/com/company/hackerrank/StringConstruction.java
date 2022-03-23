ab3474ac0f1df2bef1b0cb854bb2311d3e240c99
Richies Luong <richiesluong@gmail.com>
2022-03-22 21:03:18 -0400

    Not Solved

--- src/com/company/hackerrank/StringConstruction.java
+++ src/com/company/hackerrank/StringConstruction.java
@@ -1,2 +1,26 @@
-package com.company.hackerrank;public class StringConstruction {
+package com.company.hackerrank;
+
+public class StringConstruction {
+
+    public static int stringConstruction(String s) {
+
+        int cost = 0;
+        String p = "";
+        String[] arr = s.split("");
+
+        for (int i = 0; i < arr.length; i++) {
+
+            p += arr[i];
+            cost++;
+        }
+
+        System.out.println(p);
+        return cost;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(stringConstruction("abcd"));
+
+    }
 }
