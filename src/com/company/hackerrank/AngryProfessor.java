1ba632fbafbb0bff9e679abf81a12adfd8c2f766
Richies Luong <richiesluong@gmail.com>
2022-03-27 12:31:16 -0400

    Solved

--- src/com/company/hackerrank/AngryProfessor.java
+++ src/com/company/hackerrank/AngryProfessor.java
@@ -1,2 +1,32 @@
-package com.company.hackerrank;public class AngryProfessor {
+package com.company.hackerrank;
+
+import java.util.List;
+
+public class AngryProfessor {
+
+    public static String angryProfessor(int k, List<Integer> a) {
+
+        int onTime = 0;
+
+        for (Integer integer : a) {
+
+            if (integer <= 0) {
+                onTime++;
+            }
+        }
+        if (onTime < k) {
+            return "YES";
+        }
+        return "NO";
+    }
+
+    public static void main(String[] args) {
+
+        List<Integer> test = List.of(-1, -3, 4, 2);
+        List<Integer> test2 = List.of(0, -1, 2, 1);
+
+        System.out.println(angryProfessor(3, test));
+        System.out.println(angryProfessor(2, test2));
+
+    }
 }
