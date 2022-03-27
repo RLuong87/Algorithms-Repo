ba256b740cbac20cc35cd515b4233eedbc9bbcca
Richies Luong <richiesluong@gmail.com>
2022-03-27 19:33:57 -0400

    Not solved

--- src/com/company/hackerrank/ViralAdvertising.java
+++ src/com/company/hackerrank/ViralAdvertising.java
@@ -1,2 +1,32 @@
-package com.company.hackerrank;public class ViralAdvertising {
+package com.company.hackerrank;
+
+import java.util.Arrays;
+
+public class ViralAdvertising {
+
+    /*
+
+
+
+     */
+
+    public static int viralAdvertising(int n) {
+
+        int[] days = new int[n];
+        int people = 5;
+
+        for (int i = 0; i <= days.length; i++) {
+            people = people / 2;
+
+//            people++;
+//            System.out.println(Arrays.toString(days));
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(viralAdvertising(5));
+
+    }
 }
