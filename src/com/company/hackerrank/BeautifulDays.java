1fef160b3b601cfcb699d5219e70c4e0eee7c54b
Richies Luong <richiesluong@gmail.com>
2022-03-27 13:02:56 -0400

    Solved

--- src/com/company/hackerrank/BeautifulDays.java
+++ src/com/company/hackerrank/BeautifulDays.java
@@ -1,39 +1,33 @@
 package com.company.hackerrank;
 
-import java.util.*;
-
 public class BeautifulDays {
+
+    /*
+
+    ****** SOLVED *******
+
+     */
 
     public static int beautifulDays(int i, int j, int k) {
 
-        List<Integer> numbers = new ArrayList<>();
-        int beautifulDay = 0, rev = 0;
+        int beautifulDays = 0;
 
         for (int l = i; l <= j; l++) {
+            StringBuilder str = new StringBuilder();
+            str.append(l);
+            str.reverse();
+            int a = Integer.parseInt(String.valueOf(str));
 
-            numbers.add(l);
-
-            for (int m = numbers.size() - 1; m >= 0 ; m--) {
-
-
+            if ((l - a) % k == 0) {
+                beautifulDays++;
             }
-
         }
-
-
-        return 0;
+        return beautifulDays;
     }
 
     public static void main(String[] args) {
 
         System.out.println(beautifulDays(20, 23, 6));
 
-        int num = 23, rev = 0;
-
-        for (; num != 0; num = num / 10) {
-            int remainder = num % 10;
-            rev = rev * 10 + remainder;
-        }
-        System.out.println(rev);
     }
 }
