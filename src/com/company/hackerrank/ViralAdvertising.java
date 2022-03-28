7e8b7533991018186661a32f370acdd570f1b11e
Richies Luong <richiesluong@gmail.com>
2022-03-28 18:35:34 -0400

    Solved

--- src/com/company/hackerrank/ViralAdvertising.java
+++ src/com/company/hackerrank/ViralAdvertising.java
@@ -13,10 +13,10 @@ public class ViralAdvertising {
     public static int viralAdvertising(int n) {
 
         int[] days = new int[n];
-        int people = 5, liked, totalLikes = 0;
+        int people = 5, totalLikes = 0;
 
         for (int i = 0; i < days.length; i++) {
-            liked = people / 2;
+            int liked = people / 2;
             people = people / 2 * 3;
             totalLikes += liked;
         }
