e213216b835939cbb14fa010297aabe5e76dc82c
Richies Luong <richiesluong@gmail.com>
2022-03-27 20:14:46 -0400

    Solved

--- src/com/company/hackerrank/ViralAdvertising.java
+++ src/com/company/hackerrank/ViralAdvertising.java
@@ -6,27 +6,30 @@ public class ViralAdvertising {
 
     /*
 
+     ***** SOLVED *****
 
-
-     */
+    */
 
     public static int viralAdvertising(int n) {
 
         int[] days = new int[n];
-        int people = 5;
+        int people = 5, liked, totalLikes = 0;
 
-        for (int i = 0; i <= days.length; i++) {
-            people = people / 2;
-
-//            people++;
-//            System.out.println(Arrays.toString(days));
+        for (int i = 0; i < days.length; i++) {
+            liked = people / 2;
+            people = people / 2 * 3;
+            totalLikes += liked;
         }
-        return 0;
+        return totalLikes;
     }
 
     public static void main(String[] args) {
 
+        System.out.println(viralAdvertising(3));
+        System.out.println(viralAdvertising(4));
         System.out.println(viralAdvertising(5));
-
+        System.out.println(viralAdvertising(50));
+        System.out.println(viralAdvertising(49));
+        System.out.println(viralAdvertising(34));
     }
 }
