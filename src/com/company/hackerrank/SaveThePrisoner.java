d953c8f330fc1aeda8fb484383ea6eb771a6f2a3
Richies Luong <richiesluong@gmail.com>
2022-03-30 10:05:58 -0400

    Not Solved

--- src/com/company/hackerrank/SaveThePrisoner.java
+++ src/com/company/hackerrank/SaveThePrisoner.java
@@ -2,37 +2,36 @@ package com.company.hackerrank;
 
 public class SaveThePrisoner {
 
-    public static int saveThePrisoner(int n, int m, int s) {
+    public static int saveThePrisoner(int prisoners, int treats, int start) {
 
-        int[] prisoners = new int[n + 1];
-        int i = 0;
+        int chair;
 
         while (true) {
-            for (i = s; i < prisoners.length; i++) {
-                System.out.print(i + " ");
-                m--;
-                if (m == 0) {
+            for (chair = start; chair < prisoners + 1; chair++) {
+                System.out.println(chair + " " + treats);
+                treats--;
+                if (treats == 0) {
                     System.out.println();
                     System.out.print("Warn prisoner in chair #");
-                    return i;
+                    return chair;
                 }
-                s = 1;
             }
+            start = 1;
         }
     }
 
     public static void main(String[] args) {
 
-        System.out.println(saveThePrisoner(4, 6, 2));
-        System.out.println("___________________");
-        System.out.println(saveThePrisoner(5, 2, 1));
-        System.out.println("___________________");
-        System.out.println(saveThePrisoner(5, 2, 2));
-        System.out.println("___________________");
-        System.out.println(saveThePrisoner(7, 19, 2));
-        System.out.println("___________________");
-        System.out.println(saveThePrisoner(3, 7, 3));
-        System.out.println("___________________");
+//        System.out.println(saveThePrisoner(4, 6, 2)); // -> 3
+//        System.out.println("___________________");
+//        System.out.println(saveThePrisoner(5, 2, 1)); // -> 2
+//        System.out.println("___________________");
+//        System.out.println(saveThePrisoner(5, 2, 2)); // -> 3
+//        System.out.println("___________________");
+        System.out.println(saveThePrisoner(7, 19, 2)); // -> 6
+//        System.out.println("___________________");
+//        System.out.println(saveThePrisoner(3, 7, 3)); // -> 3
+//        System.out.println("___________________");
 
     }
 }
