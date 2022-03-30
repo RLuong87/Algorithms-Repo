2ca456ef1d795b8da079b07d8dbaa1e3d05e149a
Richies Luong <richiesluong@gmail.com>
2022-03-30 10:07:16 -0400

    Not Solved

--- src/com/company/hackerrank/ClimbingLeaderboard.java
+++ src/com/company/hackerrank/ClimbingLeaderboard.java
@@ -1,5 +1,6 @@
 package com.company.hackerrank;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ClimbingLeaderboard {
@@ -12,5 +13,14 @@ public class ClimbingLeaderboard {
 
     public static void main(String[] args) {
 
+//        System.out.println(climbingLeaderboard());
+        List<Integer> rankedTest = Arrays.asList(100, 100, 50, 40, 40, 20, 10);
+        //expecting 6, 4, 2, 1
+
+        for (int i = 0; i < rankedTest.size(); System.out.println(i)) {
+            rankedTest.set(i, rankedTest.get(i) * i);
+            i++;
+        }
+        System.out.println(rankedTest);
     }
 }
