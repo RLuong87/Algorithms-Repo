1616cd9c9c52eb25072d4ca06485bd3ee9fea68a
Richies Luong <richiesluong@gmail.com>
2022-03-30 10:06:34 -0400

    Solved

--- src/com/company/hackerrank/ViralAdvertising.java
+++ src/com/company/hackerrank/ViralAdvertising.java
@@ -12,13 +12,14 @@ public class ViralAdvertising {
 
     public static int viralAdvertising(int n) {
 
-        int[] days = new int[n];
         int people = 5, totalLikes = 0;
 
-        for (int i = 0; i < days.length; i++) {
+        for (int i = 1; i < n + 1; i++) {
             int liked = people / 2;
             people = people / 2 * 3;
             totalLikes += liked;
+            System.out.println("Day Shared  Liked  Cumulative");
+            System.out.println(i + "     " + people + "     " + liked + "        " + totalLikes);
         }
         return totalLikes;
     }
@@ -26,10 +27,12 @@ public class ViralAdvertising {
     public static void main(String[] args) {
 
         System.out.println(viralAdvertising(3));
+        System.out.println();
         System.out.println(viralAdvertising(4));
+        System.out.println();
         System.out.println(viralAdvertising(5));
-        System.out.println(viralAdvertising(50));
-        System.out.println(viralAdvertising(49));
-        System.out.println(viralAdvertising(34));
+//        System.out.println(viralAdvertising(50));
+//        System.out.println(viralAdvertising(49));
+//        System.out.println(viralAdvertising(34));
     }
 }
