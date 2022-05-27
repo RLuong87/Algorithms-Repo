664db3694ef500ad33750e406709a8339dfde2dc
Richies Luong <richiesluong@gmail.com>
2022-05-27 11:06:03 -0400

    HackerEarth package added

---
+++ src/com/company/hackerearth/CalculateCost.java
@@ -0,0 +1,27 @@
+package com.company.hackerearth;
+
+import java.util.Arrays;
+
+public class CalculateCost {
+
+    static long getCost(int X, int k, int[] price, int N){
+
+        int cost = 0;
+//        price = new int[N];
+
+        for (int i = 0; i < price.length - 1; i++) {
+
+
+        }
+        System.out.println(Arrays.toString(price));
+        return cost;
+    }
+
+    public static void main(String[] args) {
+
+        int[] price = {1, 2, 3, 1};
+
+        System.out.println(getCost(5, 2, price, price.length));
+
+    }
+}
