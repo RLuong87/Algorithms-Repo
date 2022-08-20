cf1cd339eef54846df82278fb739cdf3e61f7810
Richies Luong <richiesluong@gmail.com>
2022-08-20 13:11:17 -0400

    Breaking the Records

--- src/com/company/hackerrank/interviewprepkit/BreakingTheRecords.java
+++ src/com/company/hackerrank/interviewprepkit/BreakingTheRecords.java
@@ -1,2 +1,21 @@
-package com.company.hackerrank.interviewprepkit;public class BreakingTheRecords {
+package com.company.hackerrank.interviewprepkit;
+
+import java.util.List;
+
+public class BreakingTheRecords {
+
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+        // Write your code here
+        int minCount = 0, maxCount = 0;
+        for (int i = 0; i < scores.size() - 1; i++) {
+        }
+        System.out.println(scores);
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        List<Integer> test1 = List.of(10, 5, 20, 20, 4, 5, 2, 25, 1);
+        System.out.println(breakingRecords(test1));
+    }
 }
