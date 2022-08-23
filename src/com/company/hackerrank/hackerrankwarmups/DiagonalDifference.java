bcf6fe1f65f9f67c7c3821fd340da875984fc5e0
Richies Luong <richiesluong@gmail.com>
2022-08-23 18:36:56 -0400

    Diagonal Difference

--- src/com/company/hackerrank/hackerrankwarmups/DiagonalDifference.java
+++ src/com/company/hackerrank/hackerrankwarmups/DiagonalDifference.java
@@ -7,25 +7,29 @@ public class DiagonalDifference {
 
     public static int diagonalDifference(List<List<Integer>> arr) {
 
-        int leftToRight = 0, rightToLeft = 0;
+        int leftToRight = 0, rightToLeft = 0, N = arr.get(0).size() - 1;
 
-        for (int i = 0; i < arr.size(); i++) {
-
-           leftToRight = arr.get(0).get(0) + arr.get(1).get(1) + arr.get(2).get(2);
-           rightToLeft = arr.get(0).get(2) + arr.get(1).get(1) + arr.get(2).get(0);
-
+        for (int i = 0; i < arr.size(); i++, N--) {
+            leftToRight += arr.get(i).get(i);
+            rightToLeft += arr.get(i).get(N);
         }
         return Math.abs(leftToRight - rightToLeft);
     }
 
     public static void main(String[] args) {
 
-        List<List<Integer>> arr = new ArrayList<>();
-        arr.add(List.of(1, 2, 3));
-        arr.add(List.of(4, 5, 6));
-        arr.add(List.of(9, 8, 9));
+        List<List<Integer>> test1 = new ArrayList<>();
+        test1.add(List.of(11, 2, 4));
+        test1.add(List.of(4, 5, 6));
+        test1.add(List.of(10, 8, -12));
+//        System.out.println(diagonalDifference(test1));
 
-        System.out.println(diagonalDifference(arr));
-
+        List<List<Integer>> test2 = new ArrayList<>();
+        test2.add(List.of(-10, 3, 0, 5, -4));
+        test2.add(List.of(2, -1, 0, 2, -8));
+        test2.add(List.of(9, -2, -5, 6, 0));
+        test2.add(List.of(9, -7, 4, 8, -2));
+        test2.add(List.of(3, 7, 8, -5, 0));
+        System.out.println(diagonalDifference(test2));
     }
 }
---
+++ src/com/company/hackerrank/interviewprepkit/JAVA/JavaLoops1.java
@@ -0,0 +1,2 @@
+package com.company.hackerrank.interviewprepkit.JAVA;public class JavaLoops1 {
+}
