24d8f34247a4d23d5842e29a122134b76979db85
Richies Luong <richiesluong@gmail.com>
2022-08-22 20:38:47 -0400

    Breaking the Records

--- src/com/company/hackerrank/interviewprepkit/WEEK1/BreakingTheRecords.java
+++ src/com/company/hackerrank/interviewprepkit/WEEK1/BreakingTheRecords.java
@@ -1,16 +1,33 @@
 package com.company.hackerrank.interviewprepkit.WEEK1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class BreakingTheRecords {
 
     public static List<Integer> breakingRecords(List<Integer> scores) {
         // Write your code here
-        int minCount = 0, maxCount = 0;
-        for (int i = 0; i < scores.size() - 1; i++) {
+        int min = scores.get(0), max = scores.get(0), minCount = 0, maxCount = 0;
+
+        for (int i = 1; i < scores.size(); i++) {
+            if (scores.get(i) < min) {
+                min = scores.get(i);
+                minCount++;
+            }
+
+            if (scores.get(i) > max) {
+                max = scores.get(i);
+                maxCount++;
+            }
+//            System.out.println("Min count = " + minCount);
+            System.out.println("Max count = " + maxCount);
+
         }
-        System.out.println(scores);
-        return null;
+        System.out.println(max);
+        System.out.println(maxCount);
+
+        return new ArrayList<>(Arrays.asList(maxCount, minCount));
     }
 
     public static void main(String[] args) {
