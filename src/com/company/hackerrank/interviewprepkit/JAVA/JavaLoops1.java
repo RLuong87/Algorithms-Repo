ffbdb547d4ff2080896f1bc34497698f553278c3
Richies Luong <richiesluong@gmail.com>
2022-08-23 18:37:34 -0400

    Java Loops 1

--- src/com/company/hackerrank/interviewprepkit/JAVA/JavaLoops1.java
+++ src/com/company/hackerrank/interviewprepkit/JAVA/JavaLoops1.java
@@ -1,2 +1,19 @@
-package com.company.hackerrank.interviewprepkit.JAVA;public class JavaLoops1 {
+package com.company.hackerrank.interviewprepkit.JAVA;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JavaLoops1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println(N + " x " + i + " = " + (N * i));
+        }
+        bufferedReader.close();
+    }
 }
