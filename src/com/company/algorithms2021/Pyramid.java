4254f720aae562dee77c925fda93ca45fdd4cb98
Richies Luong <richiesluong@gmail.com>
2022-08-24 12:41:23 -0400

    Pyramid algorithm

--- src/com/company/algorithms2021/Pyramid.java
+++ src/com/company/algorithms2021/Pyramid.java
@@ -2,26 +2,23 @@ package com.company.algorithms2021;
 
 public class Pyramid {
 
-    public static void pyramid() {
+    public static void pyramid(int stairSize) {
 
-//      Simple Pyramid
+        for (int i = 0; i < stairSize; i++) {
 
-        int pSize = 10;
-
-        for (int i = 0; i < pSize; i++) {
-
-            for (int j = pSize - 1; j > i; j--) {
+            for (int j = stairSize - 1; j > i; j--) {
 
                 System.out.print(" ");
             }
-            System.out.print(" ".repeat(pSize - i - 1));
 
-//            System.out.println("*".repeat(2 * i + 1));
-
-            for (int j = 0; j < 2 * i + 1; j++) {
-                System.out.print('*');
+            for (int j = 0; j < 1 + i; j++) {
+                System.out.print('#');
             }
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        pyramid(6);
     }
 }
