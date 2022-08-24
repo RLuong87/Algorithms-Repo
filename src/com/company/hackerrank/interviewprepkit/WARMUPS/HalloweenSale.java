96a890a588444fe20b581d3b35349e797d1e9b6c
Richies Luong <richiesluong@gmail.com>
2022-08-24 12:40:23 -0400

    Halloween Sale

--- src/com/company/hackerrank/interviewprepkit/WARMUPS/HalloweenSale.java
+++ src/com/company/hackerrank/interviewprepkit/WARMUPS/HalloweenSale.java
@@ -1,2 +1,29 @@
-package com.company.hackerrank.interviewprepkit.WARMUPS;public class HalloweenSale {
+package com.company.hackerrank.interviewprepkit.WARMUPS;
+
+import java.util.List;
+
+public class HalloweenSale {
+
+    public static int howManyGames(int p, int d, int m, int s) {
+        // Return the number of games you can buy
+        int amountOfGames = 0, total = 0, discount = 0;
+
+        for (int i = p; i > 0; i--) {
+            p -= d;
+//            System.out.println(p);
+            System.out.println(p);
+            amountOfGames++;
+            if (p <= m) {
+                break;
+            }
+        }
+        return amountOfGames;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(howManyGames(20,3,6,70));
+
+        System.out.println(howManyGames(20, 3, 6, 85));
+
+    }
 }
