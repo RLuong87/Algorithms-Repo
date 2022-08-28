08f90e5f7c17302c0defd3afb1fc7083714e4178
Richies Luong <richiesluong@gmail.com>
2022-08-28 16:01:20 -0400

    Number Line Jumps

--- src/com/company/hackerrank/interviewprepkit/IMPLEMENTATION/NumberLineJumps.java
+++ src/com/company/hackerrank/interviewprepkit/IMPLEMENTATION/NumberLineJumps.java
@@ -4,18 +4,15 @@ public class NumberLineJumps {
 
     public static String kangaroo(int x1, int v1, int x2, int v2) {
 
-        int firstJumpX1 = x1 + v1, secondJumpX2 = x2 + v2;
-        for (int i = 0; i < 10000; i++) {
-            if (firstJumpX1 == secondJumpX2) {
-                return "YES";
-            }
+        int jumps = v1 - v2, kangaroos = x1 - x2;
+        if (v1 > v2 && kangaroos % jumps == 0) {
+            return "YES";
         }
         return "NO";
     }
 
     public static void main(String[] args) {
-//        System.out.println(kangaroo(0, 3, 4, 2));
-
-        System.out.println(Math.pow(1, 2));
+        System.out.println(kangaroo(0, 3, 4, 2));
+        System.out.println(kangaroo(0, 2, 5, 3));
     }
 }
