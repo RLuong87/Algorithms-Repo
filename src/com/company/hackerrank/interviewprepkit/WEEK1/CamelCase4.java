bbb796d77ec5bd1bbfa324219d1a81a009358610
Richies Luong <richiesluong@gmail.com>
2022-08-29 22:38:20 -0400

    Camel Case 4

--- src/com/company/hackerrank/interviewprepkit/WEEK1/CamelCase4.java
+++ src/com/company/hackerrank/interviewprepkit/WEEK1/CamelCase4.java
@@ -1,65 +1,57 @@
 package com.company.hackerrank.interviewprepkit.WEEK1;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class CamelCase4 {
 
     public static void main(String[] args) {
-
-
         Scanner scan = new Scanner(System.in);
 
-        StringBuilder str = new StringBuilder();
-        StringBuilder newStr = new StringBuilder();
-        String input = scan.next();
+        while (scan.hasNext()) {
+            String input = scan.nextLine();
 
-        for (int i = 0; i < input.length(); i++) {
+            StringBuilder str = new StringBuilder();
             if (input.startsWith("S;M;") && input.endsWith("()")) {
-                input = input.substring(4, input.length() - 2);
-                str.append(input);
+                str.append(input.substring(4, input.length() - 2));
             } else if (input.startsWith("S;C;") || input.startsWith("S;V;")) {
-                input = input.substring(4);
-                str.append(input);
-            }
-        }
-
-        for (int i = 0; i < str.length(); i++) {
-            char c = input.charAt(i);
-            if (i != 0 && Character.isUpperCase(c)) {
-                newStr.append(' ');
-                newStr.append(Character.toLowerCase(c));
-            } else if (i == 0 && Character.isUpperCase(c)) {
-                newStr.append(' ');
-                newStr.append(Character.toLowerCase(c));
+                str.append(input.substring(4));
+            } else if (input.startsWith("C;C;")) {
+                str.append(input.substring(2).toLowerCase());
+                System.out.println(str);
+            } else if (input.startsWith("C;M;")) {
+                str.append(input.substring(2).toLowerCase());
+                System.out.println(str);
             } else {
-                newStr.append(c);
+                str.append(input.substring(2).toLowerCase());
             }
-        }
-        System.out.println(newStr.toString().
-
-                trim());
-
-        char[] chars = input.toCharArray();
-
-//        for (int i = 4; i < chars.length; i++) {
-//            if (Character.isUpperCase(chars[i])) {
-//                space.append(" ");
-//                chars[i] = Character.toLowerCase(chars[i]);
-//            }
-//            str.append(space).append(chars[i]);
-//        }
-//        System.out.println(str);
-
-        for (
-                char c : input.toCharArray()) {
-            if (Character.isUpperCase(c)) {
-                c = Character.toLowerCase(c);
+            StringBuilder newStr = new StringBuilder();
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+                if (i != 0 && Character.isUpperCase(c)) {
+                    newStr.append(' ');
+                    newStr.append(Character.toLowerCase(c));
+                } else if (i == 0 && Character.isUpperCase(c)) {
+                    newStr.append(' ');
+                    newStr.append(Character.toLowerCase(c));
+                } else if (str.charAt(0) == 'c') {
+                    String[] arr = str.toString().split(" ");
+                    for (String s : arr) {
+                        newStr.append(s.toUpperCase().charAt(0)).append(s.substring(3));
+                        // System.out.println(s.toUpperCase().charAt(0) + s.substring(3));
+                    }
+                } else if (str.charAt(0) == 'm') {
+                    newStr.append(Character.toUpperCase(c));
+                } else if (str.charAt(0) == 'v') {
+                    newStr.append(Character.toUpperCase(c));
+                } else {
+                    newStr.append(c);
+                }
             }
-//            space.append(c);
+            System.out.println(newStr.toString().trim());
         }
-//        System.out.println(space);
         scan.close();
     }
 }
