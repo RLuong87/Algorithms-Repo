4b98fa1dd72485f7cbc4e1ae8846defca30226c1
Richies Luong <richiesluong@gmail.com>
2022-08-31 23:19:24 -0400

    Camel Case 4

--- src/com/company/hackerrank/interviewprepkit/WEEK1/CamelCase4.java
+++ src/com/company/hackerrank/interviewprepkit/WEEK1/CamelCase4.java
@@ -10,46 +10,62 @@ public class CamelCase4 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        while (scan.hasNext()) {
+        while (scan.hasNextLine()) {
             String input = scan.nextLine();
 
             StringBuilder str = new StringBuilder();
+            StringBuilder newStr = new StringBuilder();
             if (input.startsWith("S;M;") && input.endsWith("()")) {
                 str.append(input.substring(4, input.length() - 2));
+                for (int i = 0; i < str.length(); i++) {
+                    char c = str.charAt(i);
+                    if (i != 0 && Character.isUpperCase(c)) {
+                        newStr.append(' ');
+                        newStr.append(Character.toLowerCase(c));
+                    } else if (i == 0 && Character.isUpperCase(c)) {
+                        newStr.append(' ');
+                        newStr.append(Character.toLowerCase(c));
+                    } else {
+                        newStr.append(c);
+                    }
+                }
             } else if (input.startsWith("S;C;") || input.startsWith("S;V;")) {
                 str.append(input.substring(4));
-            } else if (input.startsWith("C;C;")) {
-                str.append(input.substring(2).toLowerCase());
-                System.out.println(str);
-            } else if (input.startsWith("C;M;")) {
-                str.append(input.substring(2).toLowerCase());
-                System.out.println(str);
-            } else {
-                str.append(input.substring(2).toLowerCase());
-            }
-            StringBuilder newStr = new StringBuilder();
-            for (int i = 0; i < str.length(); i++) {
-                char c = str.charAt(i);
-                if (i != 0 && Character.isUpperCase(c)) {
-                    newStr.append(' ');
-                    newStr.append(Character.toLowerCase(c));
-                } else if (i == 0 && Character.isUpperCase(c)) {
-                    newStr.append(' ');
-                    newStr.append(Character.toLowerCase(c));
-                } else if (str.charAt(0) == 'c') {
-                    String[] arr = str.toString().split(" ");
-                    for (String s : arr) {
-                        newStr.append(s.toUpperCase().charAt(0)).append(s.substring(3));
-                        // System.out.println(s.toUpperCase().charAt(0) + s.substring(3));
+                for (int i = 0; i < str.length(); i++) {
+                    char c = str.charAt(i);
+                    if (i != 0 && Character.isUpperCase(c)) {
+                        newStr.append(' ');
+                        newStr.append(Character.toLowerCase(c));
+                    } else if (i == 0 && Character.isUpperCase(c)) {
+                        newStr.append(' ');
+                        newStr.append(Character.toLowerCase(c));
+                    } else {
+                        newStr.append(c);
                     }
-                } else if (str.charAt(0) == 'm') {
-                    newStr.append(Character.toUpperCase(c));
-                } else if (str.charAt(0) == 'v') {
-                    newStr.append(Character.toUpperCase(c));
-                } else {
-                    newStr.append(c);
                 }
+            } else if (input.startsWith("C;C;")) {
+                str.append(input.substring(4));
+                String[] arr = str.toString().split(" ");
+                for (int i = 0; i < arr.length; i++) {
+                    arr[i] = arr[i].substring(0, 1).toUpperCase() + arr[i].substring(1);
+                }
+                for (String s : arr) {
+                    newStr.append(s);
+                }
+            } else if (input.startsWith("C;M;")) {
+                str.append(input.substring(4));
+                String[] arr = str.toString().split(" ");
+                for (int i = 1; i < arr.length; i++) {
+                    arr[i] = arr[i].substring(0, 1).toUpperCase() + arr[i].substring(1);
+                }
+                for (String s : arr) {
+                    newStr.append(s);
+                }
+                newStr.append("()");
+            } else {
+                str.append(input.substring(4));
             }
+            // String[] arr = str.toString().split("(?=[A-Z])");
             System.out.println(newStr.toString().trim());
         }
         scan.close();
