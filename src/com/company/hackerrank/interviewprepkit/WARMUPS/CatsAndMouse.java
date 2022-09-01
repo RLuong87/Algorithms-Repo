0b500a13743ac5c5e64313f5b0fe1d1df84c2958
Richies Luong <richiesluong@gmail.com>
2022-09-01 19:43:27 -0400

    Cats and a Mouse HackerRank

--- src/com/company/hackerrank/interviewprepkit/WARMUPS/CatsAndMouse.java
+++ src/com/company/hackerrank/interviewprepkit/WARMUPS/CatsAndMouse.java
@@ -1,4 +1,18 @@
 package com.company.hackerrank.interviewprepkit.WARMUPS;
 
 public class CatsAndMouse {
+
+    static String catAndMouse(int x, int y, int z) {
+
+        int m = Math.abs(x - y), n = Math.abs(z - y), k = Math.abs(z - x);
+
+        if (n < k || m < k) return "Cat B";
+        else if (m == n || k == m || n == k) return "Mouse C";
+        return "Cat A";
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(catAndMouse(1, 2, 3));
+    }
 }
