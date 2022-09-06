8920512cec7e75db4165988b46ca36cd4a4fd0e6
Richies Luong <richiesluong@gmail.com>
2022-09-05 23:31:47 -0400

    Electronics Shop HackerRank

---
+++ src/com/company/hackerrank/interviewprepkit/IMPLEMENTATION/ElectronicsShop.java
@@ -0,0 +1,15 @@
+package com.company.hackerrank.interviewprepkit.IMPLEMENTATION;
+
+public class ElectronicsShop {
+
+    static int getMoneySpent(int[] keyboards, int[] drives, int b) {
+        /*
+         * Write your code here.
+         */
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
