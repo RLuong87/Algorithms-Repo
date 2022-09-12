f8d2f51465028cd27ad8a468eb315f2ac5a4e1c6
Richies Luong <richiesluong@gmail.com>
2022-09-12 12:53:55 -0400

    Type of Triangle

---
+++ src/com/company/hackerrank/interviewprepkit/SQL/TypeOfTriangle.java
@@ -0,0 +1,12 @@
+package com.company.hackerrank.interviewprepkit.SQL;
+
+public class TypeOfTriangle {
+
+//    SELECT CASE WHEN A+B<=C OR B+C<=A THEN 'Not A Triangle'
+//    WHEN A=B AND B=C THEN 'Equilateral'
+//    WHEN A=B OR B=C OR C=A THEN 'Isosceles'
+//    ELSE 'Scalene'
+//    END
+//    FROM TRIANGLES;
+
+}
