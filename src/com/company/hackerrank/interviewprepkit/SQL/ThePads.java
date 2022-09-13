3a6a3512a79cb3db6c38e2fb7b045db26a67c474
Richies Luong <richiesluong@gmail.com>
2022-09-13 18:42:20 -0400

    The Pads HackerRank SQL problem

---
+++ src/com/company/hackerrank/interviewprepkit/SQL/ThePads.java
@@ -0,0 +1,19 @@
+package com.company.hackerrank.interviewprepkit.SQL;
+
+public class ThePads {
+
+//    SELECT NAME,CONCAT('(',SUBSTRING(Occupation,1,1),')')
+//    FROM OCCUPATIONS
+//    WHERE (Occupation = 'Singer')
+//    ORDER BY Name ASC;
+//
+//    SELECT COUNT(Occupation) FROM OCCUPATIONS
+//    WHERE (Occupation = 'Singer');
+//
+//-- SELECT CASE WHEN Occupation = 'Doctor' THEN 'There are a total doctors'
+//            --             WHEN Occupation = 'Actor' THEN 'There are a total actors'
+//            --             WHEN Occupation = 'Singer' THEN 'There are a total singers'
+//            --             WHEN Occupation = 'Professor' THEN 'There are a total professors'
+//            --             END
+//--             FROM OCCUPATIONS;
+}
