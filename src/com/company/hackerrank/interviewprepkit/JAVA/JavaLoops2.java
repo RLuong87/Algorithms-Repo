849def6f10953969c809a4cee9203b63d4df9812
Richies Luong <richiesluong@gmail.com>
2022-09-15 23:01:19 -0400

    Java Loops 2

---
+++ src/com/company/hackerrank/interviewprepkit/JAVA/JavaLoops2.java
@@ -0,0 +1,17 @@
+package com.company.hackerrank.interviewprepkit.JAVA;
+
+import java.util.Scanner;
+
+public class JavaLoops2 {
+
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+        }
+        in.close();
+    }
+}
